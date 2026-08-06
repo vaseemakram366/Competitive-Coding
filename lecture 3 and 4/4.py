ab9b6255d8c7f7f9e87befd95aad59d72cf890f9
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-06 21:20:27 +0530

    Solved Nth fibonacci number

--- lecture 3 and 4/4.py
+++ lecture 3 and 4/4.py
@@ -1,2 +1,12 @@
 # Nth Fibonacci Number
 
+class Solution:
+    def nthFibonacci(self, n: int) -> int:
+        # Base cases
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+
+        # Recursive case
+        return self.nthFibonacci(n - 1) + self.nthFibonacci(n - 2)
