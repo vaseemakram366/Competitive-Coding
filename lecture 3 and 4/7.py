41e725f59615718362ad8295cfd6064dc08b147d
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-11 23:57:57 +0530

    Add two numbers by user input

--- lecture 3 and 4/7.py
+++ lecture 3 and 4/7.py
@@ -1,6 +1,13 @@
 # add two numbers
 
-num1 = 30
-num2 = 40
+# num1 = 30
+# num2 = 40
 
-print("the sum of given two numbers is" , num1+num2)
+# print("the sum of given two numbers is" , num1+num2)
+
+
+num1 = float(input("enter a number here:"))
+num1 = float(input("enter another number here:"))
+
+sum = num1+num2
+print("the sum of the provided two numbers is", sum)
