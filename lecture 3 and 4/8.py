dcf1cbd6c806e8c69f764535e25f75778d75c9c3
Vaseem Akram <vaseemakrama366@gmail.com>
2026-08-12 23:07:43 +0530

    Tower of Hanoi

--- lecture 3 and 4/8.py
+++ lecture 3 and 4/8.py
@@ -1,1 +1,21 @@
 # solving tower of hanoi
+
+def tower_of_hanoi(n, source, auxiliary, destination):
+    if n == 1:
+        print(f"Move disk 1 from {source} to {destination}")
+        return
+
+    # Move n-1 disks from source to auxiliary
+    tower_of_hanoi(n - 1, source, destination, auxiliary)
+
+    # Move nth disk from source to destination
+    print(f"Move disk {n} from {source} to {destination}")
+
+    # Move n-1 disks from auxiliary to destination
+    tower_of_hanoi(n - 1, auxiliary, source, destination)
+
+
+# Number of disks
+n = 3
+
+tower_of_hanoi(n, 'A', 'B', 'C')
